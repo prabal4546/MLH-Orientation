36bbfc31767d8525d55cb9b04be1c7d6e36d6f02
Iqrah Nadeem <iiqrah01@gmail.com>
2022-09-21 20:13:29 +0300

    Use Canvas to draw

--- OrientationHack/PencilKitTab/CanvasView.swift
+++ OrientationHack/PencilKitTab/CanvasView.swift
@@ -5,11 +5,46 @@
 //  Created by Prabaljit Walia on 21/09/22.
 //
 
+
 import SwiftUI
 
+struct Line {
+    var points = [CGPoint]()
+    var color: Color = .red
+    var lineWidth: Double = 1.0
+}
+
 struct CanvasView: View {
+    
+    @State private var currentLine = Line()
+        @State private var lines: [Line] = []
+        @State private var thickness: Double = 1.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            
+            Canvas {context, size in
+                
+                for line in lines {
+                                    var path = Path()
+                                    path.addLines(line.points)
+                                    context.stroke(path, with: .color(line.color), lineWidth: line.lineWidth)
+                                }
+                
+            }.gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                .onChanged({ value in
+                    let newPoint = value.location
+                    currentLine.points.append(newPoint)
+                    self.lines.append(currentLine)
+                  })
+                .onEnded({ value in
+                    self.lines.append(currentLine)
+                    self.currentLine = Line(points: [], color: currentLine.color, lineWidth: thickness)
+                })
+                )
+        }
+        
     }
 }
 
