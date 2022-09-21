fe217d94fae9d62c25183acfe46778636b470639
Prabaljit Walia <prabaljitwalia4546@gmail.com>
2022-09-21 17:39:43 +0530

    Tab folders added

---
+++ OrientationHack/PencilKitTab/PencilKitTest.swift
@@ -0,0 +1,8 @@
+//
+//  PencilKitTest.swift
+//  OrientationHack
+//
+//  Created by Prabaljit Walia on 21/09/22.
+//
+
+import Foundation
---
+++ OrientationHack/SuggestionTab/STest.swift
@@ -0,0 +1,8 @@
+//
+//  STest.swift
+//  OrientationHack
+//
+//  Created by Prabaljit Walia on 21/09/22.
+//
+
+import Foundation
