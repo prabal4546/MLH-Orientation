a05ec7ad56ecec9ff84860fc5e20bfdef51f6bd1
Prabaljit Walia <prabaljitwalia4546@gmail.com>
2022-09-21 17:34:51 +0530

    first commit

---
+++ OrientationHack/OrientationHackApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrientationHackApp.swift
+//  OrientationHack
+//
+//  Created by Prabaljit Walia on 21/09/22.
+//
+
+import SwiftUI
+
+@main
+struct OrientationHackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
